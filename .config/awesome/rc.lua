0d252e626c52f71f921be33944f46cdd0712660f
Emile Turcotte <dev@emileturcotte.com>
2021-12-25 14:25:26 -0500

    Launch programs at the end of the config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -58,23 +58,6 @@ end
 
 -- }}}
 
--- {{{ Autostart windowless processes
-
--- This function will run once every time Awesome is started
-local function run_once(cmd_arr)
-    for _, cmd in ipairs(cmd_arr) do
-        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
-    end
-end
-
-run_once({ 
-	"picom --experimental-backends --config ~/.config/picom/picom.conf",
-	"emacs --daemon &",
-	"homelayout"
-}) -- comma-separated entries
-
--- }}}
-
 -- {{{ Variable definitions
 
 local themes = {
@@ -620,5 +603,22 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- }}}
+
+-- {{{ Autostart windowless processes
+
+-- This function will run once every time Awesome is started
+local function run_once(cmd_arr)
+    for _, cmd in ipairs(cmd_arr) do
+        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+    end
+end
+
+run_once({ 
+	"picom --experimental-backends --config ~/.config/picom/picom.conf",
+	"emacs --daemon &",
+	"homelayout"
+}) -- comma-separated entries
 
 -- }}}
